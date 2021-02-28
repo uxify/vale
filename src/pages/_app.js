2a767d1f321360ea991a5447f51f20f75239a638
Rishabh Priyadarshi <rishabh.priyadarshi@kindredgroup.com>
2021-02-28 18:44:14 +0100

    Express setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,25 @@
 // pages/_app.js
-import React from 'react';
+import { ApolloClient, ApolloProvider, InMemoryCache, } from '@apollo/client';
+
 import App from 'next/app';
 import './style.css';
 import './theme.css';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+  mode: 'no-cors',
+});
 
 class CustomApp extends App {
   render() {
     const { Component, pageProps } = this.props;
 
-    return <Component {...pageProps} />;
+    return (
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />;
+      </ApolloProvider>
+    )
   }
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,48 @@
+import { gql, useQuery } from '@apollo/client';
 import DefaultLayout from '../components/Layouts';
 
+const getMainContent = gql`
+    query {
+      mainContent {
+        baseUrl
+        theme {
+          id
+          name
+          colorPrimary
+        }
+        header {
+          logo {
+            imgUrl
+            height
+            width
+          }
+          navigation {
+            pageId
+            name
+            link
+          }
+        }
+        footer {
+          topFooter
+          mainFooter
+          bottomFooter
+        }
+      }
+      page(id:3) {
+        id
+        title
+        description
+        url
+      }
+    }
+      
+  `;
+
+
 const HomePage = () => {
+
+  const { loading, error, data } = useQuery(getMainContent);
+  console.log('render', loading, error, data)
   return (
     <DefaultLayout>
       <div className='section'>
